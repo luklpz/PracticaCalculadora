829657977901a6ff7138f558ec0520705c731d49
Javi <javiermengual@live.com>
2024-11-09 21:51:06 +0100

    Progreso en el método de conversión, aún por completar

--- Calculadora/src/main/java/com/grupo3/calculadora/Conversiones.java
+++ Calculadora/src/main/java/com/grupo3/calculadora/Conversiones.java
@@ -1,10 +1,39 @@
 package main.java.com.grupo3.calculadora;
 
-public class Conversiones {
+import com.grupo3.calculadora.Escaner;
 
-    public static String binarioADecimal(String binario) {
+public class Conversiones {
+    // TODO: Implementar la lógica para convertir binario a decimal
+
+    public static String binarioADecimal() {
+        String binario;
+        boolean validado = false;
+
+        do {
+            System.out.println("Dime el binario a convertir en decimal :");
+            binario = Escaner.lector.nextLine();
+            validado = esBinario(binario);
+            if (validado){
+
+
+
+            }else {
+                System.out.println("Numero no binario introducido");
+            }
+        } while (!validado);
+
 
     }
+
+    public static boolean esBinario(String str) {
+        for (char c : str.toCharArray()) {
+            if (c != '0' && c != '1') {
+                return false; // Si encuentra un carácter que no es '0' o '1', devuelve false
+            }
+        }
+        return true; // Si todos los caracteres son válidos, devuelve true
+    }
+
 
     public static void main(String[] args) {
         System.out.println("hola");
