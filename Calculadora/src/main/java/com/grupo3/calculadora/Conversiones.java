460c566069eca57bf234f06258e4ab871ae7c84d
Javi <javiermengual@live.com>
2024-11-09 21:22:10 +0100

    Arreglado problema proyecto no configurado correctamente

---
+++ Calculadora/src/main/java/com/grupo3/calculadora/Conversiones.java
@@ -0,0 +1,12 @@
+package main.java.com.grupo3.calculadora;
+
+public class Conversiones {
+
+    public static String binarioADecimal(String binario) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+}
