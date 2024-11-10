a881805669bac432a217c02e05bbba38e37afe6b
Javi <javiermengual@live.com>
2024-11-10 23:28:46 +0100

    Añado javadoc en esBinario y implemento un primer trozo en mi metodo convertirBinarioADecimal que me permite invertir el numero binario para poder trabajar mejor con el en el resto de logica.

--- Calculadora/src/main/java/com/grupo3/calculadora/Conversiones.java
+++ Calculadora/src/main/java/com/grupo3/calculadora/Conversiones.java
@@ -5,28 +5,36 @@ import com.grupo3.calculadora.Escaner;
 public class Conversiones {
     // TODO: Implementar la lógica para convertir binario a decimal
 
-    public static String binarioADecimal() {
+    public static String convertirBinarioADecimal() {
         String binario;
+        String binarioInvertido;
         boolean validado = false;
 
         do {
             System.out.println("Dime el binario a convertir en decimal :");
             binario = Escaner.lector.nextLine();
             validado = esBinario(binario);
-            if (validado){
+            if (validado) {  //Convierte la String con el binario en una array con cada caracter.
+                StringBuilder binarioInvertidoBuilder = new StringBuilder(binario); //Creo un StringBuilder apartir de mi String Binario para poder invertirla.
+                binarioInvertido=binarioInvertidoBuilder.reverse().toString(); // Invierto el StringBuilder y convierto el resultado a String para almacenarlo en binarioInvertido.
 
 
 
-            }else {
-                System.out.println("Numero no binario introducido");
+
+            } else {
+                System.err.println("Numero no binario introducido");
             }
         } while (!validado);
 
 
     }
 
+    /**
+     * @param str Recibe como parametro un String que es el numero binario que queremos validar.
+     * @return Devuelve un booleano segun si el String es binario o no.
+     */
     public static boolean esBinario(String str) {
-        for (char c : str.toCharArray()) {
+        for (char c : str.toCharArray()) { //Recorre cada char de la String convertida en Array de caracteres
             if (c != '0' && c != '1') {
                 return false; // Si encuentra un carácter que no es '0' o '1', devuelve false
             }
