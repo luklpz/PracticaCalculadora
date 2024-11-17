ede81b12e6333d386318e938b5981db0bef36abd
luklpz <luka.lopez.j@gmail.com>
2024-11-17 16:31:26 +0100

    Añadido comentario Javadoc en el método solicitarInt de Util.java.

--- Calculadora/src/main/java/com/grupo3/calculadora/Util.java
+++ Calculadora/src/main/java/com/grupo3/calculadora/Util.java
@@ -3,6 +3,16 @@ package main.java.com.grupo3.calculadora;
 import com.grupo3.calculadora.Escaner;
 
 public class Util {
+
+    /**
+     * Solicitar y validar un "int" dentro de un rango definido.
+     *
+     * @param mensajeSolicitud "String" que se imprime para solicitar el número.
+     * @param minimo Valor mínimo válido.
+     * @param maximo Valor máximo válido.
+     * @param mensajeError "String" que se imprime al recibir un valor no válido. Tras este mensaje se repite la validación sin imprimir de nuevo el mensajeSolicitud.
+     * @return Devuelve el "int" validado dentro del rango definido.
+     */
     public static int solicitarInt(String mensajeSolicitud,int minimo , int maximo, String mensajeError) {
         int numero;
         do {
