751a7c6a3b54502dd749e1d317d5ee813d8606ea
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-11-22 00:06:24 +0100

    hecho y añadido hexadecimal a binario

--- Calculadora/src/main/java/com/grupo3/calculadora/subMenuConversiones.java
+++ Calculadora/src/main/java/com/grupo3/calculadora/subMenuConversiones.java
@@ -1,23 +1,43 @@
 package main.java.com.grupo3.calculadora;
 
+import java.util.Scanner;
+
 public class subMenuConversiones {
+
+    private static int subMenu(){
+        Scanner scanner = new Scanner(System.in);
+        int opcion = 0;
+        //String submenú
+        do {
+            System.out.println("**************************\n** CONVERSIONES DE BASE **\n**************************\n1. De decimal a binario\n2. De decimal a hexadecimal\n3. De binario a decimal\n4. De binario a hexadecimal\n5. De hexadecimal a binario\n6. De hexadecimal a decimal\n---------------------------\n0. Volver al menú principal\nElija una opción:");
+            opcion = Integer.parseInt(scanner.nextLine());
+            if (opcion < 0 || opcion >6){
+                System.err.println("ERROR. Introduzca una opción válida.");
+            }
+        }while (opcion < 0 || opcion >6);
+        return opcion;
+    }
+
     /**
      * Este metodo es el submenú de Conversiones de Bases. Imprime el menú con las opciones, lee el número introducido y según la operación eleguida llama a los metodos correspondientes
      */
     public static void subMenuConv(){
-        int subMenu;
+        int opcionSeleccionada;
+        boolean cerrar=false;
+
         do {
-            System.out.println("**************************\n** CONVERSIONES DE BASE **\n**************************\n1. De decimal a binario\n2. De decimal a hexadecimal\n3. De binario a decimal\n4. De binario a hexadecimal\n5. De hexadecimal a binario\n6. De hexadecimal a decimal\n---------------------------\n0. Volver al menú principal");
-            subMenu = Util.solicitarInt("Elija una opción:\n", 0, 6, "ERROR. El número no coincide con ninguna de las opciones, PRUEBE OTRA VEZ\n");
-            switch (subMenu) {
-                case 0 -> System.out.println("Volviendo al menú principal...");//Volver al menú principal
+            opcionSeleccionada=subMenu();
+            switch (opcionSeleccionada) {
+                case 0 -> cerrar = true;//Volver al menú principal
                 case 1 -> Conversiones.convertirDecimalABinario();//Decimal a binario
                 case 2 -> Conversiones.convertirDecimalAHexadecimal();//Decimal a hexadecimal
                 case 3 -> Conversiones.convertirBinarioADecimal();//Binario a decimal
                 case 4 -> ;//Binario a hexadecimal
-                case 5 -> ;//Hexadecimal a binario
+                case 5 -> Conversiones.convertirHexadecimalABinario();//Hexadecimal a binario
                 case 6 -> Conversiones.convertirHexadecimalADecimal();//Hexadecimal a decimal
+                default -> System.out.println();
             }
-        }while (subMenu != 0);
+        } while (!cerrar);
     }
+
 }
